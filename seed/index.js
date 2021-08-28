0ff1796f791555fdfc0471108700b145bc7711c7
Aryan <27aryanraj@gmail.com>
2021-08-28 15:32:16 +0530

    First commit

--- seed/index.js
+++ seed/index.js
@@ -3,7 +3,14 @@ const Campground = require("../models/campground");
 const cities = require("./cities");
 const { descriptors, places } = require("./seedHelpers");
 
-mongoose.connect("mongodb://localhost:27017/campgrounds", {
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const localDB = "mongodb://localhost:27017/campgrounds";
+const serverDB = process.env.DB_URL || localDB;
+
+mongoose.connect(serverDB, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -20,29 +27,21 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   await Campground.deleteMany();
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 60; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
 
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      author: "6103bacee4526d32587aac50",
+      author: "610885a976bf7f001541fecc",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam laborum quibusdam quae accusantium dolores, dolore odio consequatur necessitatibus obcaecati odit corporis dolor autem. Est similique esse dolores dignissimos. Nam, sint.",
       price,
       images: [
         {
-          url: "https://res.cloudinary.com/cloudinary-static/image/upload/v1627726158/Campgrounds/oqemcjo4rht4nuxo9ncn.jpg",
-          filename: "Campgrounds/oqemcjo4rht4nuxo9ncn",
-        },
-        {
-          url: "https://res.cloudinary.com/cloudinary-static/image/upload/v1627726158/Campgrounds/drsokqpum18l4e3qtus3.jpg",
-          filename: "Campgrounds/drsokqpum18l4e3qtus3",
-        },
-        {
-          url: "https://res.cloudinary.com/cloudinary-static/image/upload/v1627726158/Campgrounds/k3tadlzbcc8l7wznvzpe.jpg",
-          filename: "Campgrounds/k3tadlzbcc8l7wznvzpe",
+          url: "https://res.cloudinary.com/cloudinary-static/image/upload/v1627935294/Campgrounds/ekikucpuofj5vxxeoyrp.jpg",
+          filename: "Campgrounds/ekikucpuofj5vxxeoyrp",
         },
       ],
       geometry: {
